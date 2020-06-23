040e3b0c6362e3b6f18fad119c6f0f8adc1b9dfb
Antowa-qq <anton-vinsss@mail.ru>
2020-06-24 01:45:54 +0300

    feat : add template

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import 'regenerator-runtime/runtime';
 
 import './js/main';
 import './scss/main.scss';
-import Login from './js/login';
-import Registration from './js/registration';
+// import Login from './js/login';
+// import Registration from './js/registration';
 
-const login = new Login('#login');
-const registration = new Registration('#registration');
+// const login = new Login('#login');
+// const registration = new Registration('#registration');
 // eslint shows errors without console.log (no-unused-vars)
-console.log(login);
-console.log(registration);
+// console.log(login);
+// console.log(registration);
+
+
+const options = document.querySelector('.options');
+const optionSettings = document.querySelector('.options__settings');
+options.addEventListener('click', () => {
+  optionSettings.classList.toggle('options__settings_inactive');
+});
+
+const sound = document.querySelector('.audio');
+sound.addEventListener('click', () => {
+  sound.classList.toggle('audio_silence');
+});
--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,9 @@ import Api from './api';
 
 const api = new Api();
 
-api.checkLogin().then((user) => {
+api.checkLogin().then(() => {
   // Already logged in
-  console.log(user);
+  // console.log(user);
 }, () => {
   // Redirect to login
 });
