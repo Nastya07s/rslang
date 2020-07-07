4efd5fda599c8eb0873394e701717fbd2a75ee23
Tanya Tomina <tominanusha@gmail.com>
2020-07-08 00:06:02 +0300

    refactor: remove console.log

--- src/minigames/sprint/index-sprint.js
+++ src/minigames/sprint/index-sprint.js
@@ -4,6 +4,5 @@ import 'regenerator-runtime/runtime';
 import Sprint from './sprint';
 import './sprint.scss';
 
+// eslint-disable-next-line no-unused-vars
 const sprint = new Sprint('#minigame-sprint');
-// eslint shows errors without console.log (no-unused-vars)
-console.log(sprint);
