dd3152e5c9a6f95cd53940e6ba20e708b03b2c56
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-07-13 22:44:21 +0300

    refactor: merge file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-// import './js/main';
-// import './scss/main.scss';
-import Login from './js/login';
-import Registration from './js/registration';
-
-const login = new Login('#login');
-const registration = new Registration('#registration');
-// eslint shows errors without console.log (no-unused-vars)
-console.log(login);
-console.log(registration);
--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,10 +1,19 @@
 import 'app/index';
 import 'app/scss/main.scss';
 import api from 'app/js/api';
+import Login from 'app/js/login';
+import Registration from 'app/js/registration';
 import './animate';
-
 import mainPage from './mainPage';
+// import './js/main';
+// import './scss/main.scss';
 
-api.loginUser({ email: 'test3@mail.ru', password: 'QQQwww123.' }).then(() => {
+const login = new Login('#login');
+const registration = new Registration('#registration');
+// eslint shows errors without console.log (no-unused-vars)
+console.log(login);
+console.log(registration);
+
+api.loginUser({ email: 'test3@gmail.com', password: 'AAbb1234!' }).then(() => {
   mainPage.init();
 });
