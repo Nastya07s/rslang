c274ccc0233497b65dff471c52c3956c8ac15c33
Antowa-qq <anton-vinsss@mail.ru>
2020-07-13 19:48:42 +0300

    refactor: remove credentials

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -2,9 +2,16 @@ import 'app/index';
 import 'app/scss/main.scss';
 import api from 'app/js/api';
 import './animate';
-
 import mainPage from './mainPage';
 
-api.loginUser({ email: 'test3@mail.ru', password: 'QQQwww123.' }).then(() => {
+api.checkLogin().then(async (user) => {
   mainPage.init();
+
+  console.log(user);
+}, () => {
+  console.log('Couldnt login');
 });
+
+// api.loginUser({ email: 'test3@mail.ru', password: 'QQQwww123.' }).then(() => {
+//   mainPage.init();
+// });
--- src/minigames/savannah/js/main.js
+++ src/minigames/savannah/js/main.js
@@ -9,17 +9,7 @@ import Model from './model/Model';
 
 (async () => {
   try {
-    // testmail5@mail.ru
-    // $wVgg123
-
-    // test-user@mail.ru
-    // $wVgg123
-    await Api.loginUser({ email: 'test-user@mail.ru', password: '$wVgg123' });
-
     await Api.checkLogin();
-    // console.log(check);
-    // const res = await api.loginUser({ email: 'testmail5@mail.ru', password: '$wVgg123' });
-    // console.log(res);
     const app = new Controller(new Model(), new View());
     app.init();
   } catch (e) {
