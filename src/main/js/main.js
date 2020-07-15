2db640d20afeeede3567c027027cfa39f629c3e2
Alexey Shevchenko <kamikozz@mail.ru>
2020-07-15 06:12:46 +0300

    feat: add global statistics module & component with canvasjs

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -3,10 +3,18 @@ import '../scss/main.scss';
 import api from 'app/js/api';
 import './animate';
 import mainPage from './mainPage';
+// import GlobalStatistics from '../components/global-statistics';
 
-api.checkLogin().then((user) => {
+api.checkLogin().then(async (user) => {
   console.log('AUTHORIZED as', user);
   mainPage.init();
+
+  // const globalStats = new GlobalStatistics({
+  //   element: document.body,
+  // });
+
+  // await globalStats.init();
+  // globalStats.showStatistics();
 }, () => {
   console.log('UNAUTHORIZED');
 });
